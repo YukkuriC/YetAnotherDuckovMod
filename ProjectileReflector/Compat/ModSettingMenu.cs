a27891b05c8205d2e0602be2ef011746bd218c72
YukkuriC <799433638@qq.com>
2025-11-08 14:16:09 +0800

    计划添加ModSetting，顺便把这边的changelog还是删了

---
+++ ProjectileReflector/Compat/ModSettingMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Duckov.Modding;
+using SodaCraft.Localizations;
+using UnityEngine;
+using YukkuriC;
+
+namespace ProjectileReflector.Compat
+{
+    public static class ModSettingMenu
+    {
+        private static ModInfo myInfo;
+        public static void Init(ModInfo info)
+        {
+            myInfo = info;
+            ModManager.OnModActivated += OnModActivated;
+            ModManager.OnModWillBeDeactivated += OnModWillBeDeactivated;
+            if (ModSettingAPI.Init(myInfo)) AddUI();
+        }
+        public static void Dispose()
+        {
+            ModManager.OnModActivated -= OnModActivated;
+            ModManager.OnModWillBeDeactivated -= OnModWillBeDeactivated;
+        }
+
+        static void OnModActivated(ModInfo modInfo, Duckov.Modding.ModBehaviour behaviour)
+        {
+            if (modInfo.name != ModSettingAPI.MOD_NAME || !ModSettingAPI.Init(myInfo)) return;
+            if (ModSettingAPI.Init(myInfo)) AddUI();
+        }
+        static void OnModWillBeDeactivated(ModInfo modInfo, Duckov.Modding.ModBehaviour behaviour)
+        {
+            if (modInfo.name != ModSettingAPI.MOD_NAME || !ModSettingAPI.Init(myInfo)) return;
+            // TODO remove custom watcher
+        }
+
+        // TODO codegen
+        static void AddUI()
+        {
+        }
+    }
+}
