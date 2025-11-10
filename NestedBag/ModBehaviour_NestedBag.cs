2435092ec30e2737de7d034d6cb65ae0fee639e6
YukkuriC <799433638@qq.com>
2025-11-10 23:08:09 +0800

    套娃尿素袋

---
+++ NestedBag/ModBehaviour_NestedBag.cs
@@ -0,0 +1,32 @@
+﻿using ItemStatsSystem;
+using ItemStatsSystem.Items;
+
+namespace NestedBag
+{
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+        const int BAG_ID = 1255;
+        const int EXTEND_SLOTS_COUNT = 6;
+        Slot? cachedOriginal;
+
+        void OnEnable()
+        {
+            var bag = ItemAssetsCollection.GetPrefab(BAG_ID);
+            var slots = bag.Slots;
+            cachedOriginal = slots[0];
+            slots.Clear();
+            for (int i = 0; i < EXTEND_SLOTS_COUNT; i++)
+            {
+                slots.Add(new Slot($"bag_{i}"));
+            }
+        }
+        void OnDisable()
+        {
+            if (cachedOriginal == null) return;
+            var bag = ItemAssetsCollection.GetPrefab(BAG_ID);
+            var slots = bag.Slots;
+            slots.Clear();
+            slots.Add(cachedOriginal);
+        }
+    }
+}
