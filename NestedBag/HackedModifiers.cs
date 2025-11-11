95d69e7612469f1fed191961b5a2fdd28d9788c8
YukkuriC <799433638@qq.com>
2025-11-11 23:31:15 +0800

    尿素袋超强化

---
+++ NestedBag/HackedModifiers.cs
@@ -0,0 +1,51 @@
+﻿using ItemStatsSystem;
+using System.Collections.Generic;
+
+namespace NestedBag
+{
+    public class HackedModifiers : ModifierDescriptionCollection
+    {
+        StatCollection stats;
+
+        // rewrite init
+        void Awake()
+        {
+            initialized = true;
+            if (list == null) list = new List<ModifierDescription>();
+            if (master == null) master = GetComponent<Item>();
+            if (stats == null) stats = GetComponent<StatCollection>();
+
+            if (master != null)
+            {
+                master.onItemTreeChanged += OnUpdateItem;
+                master.onDurabilityChanged += OnUpdateItem;
+            }
+        }
+
+        // rewrite destroy
+        void OnDestroy()
+        {
+            if (master != null)
+            {
+                master.onItemTreeChanged -= OnUpdateItem;
+                master.onDurabilityChanged -= OnUpdateItem;
+            }
+        }
+
+        // transparent modifiers
+        void OnUpdateItem(Item master) => ReapplyModifiersOverride();
+        void ReapplyModifiersOverride()
+        {
+            if (master == null) return;
+
+            // refresh all modifiers
+            Clear();
+            foreach (var stat in stats)
+                foreach (var modifier in stat.Modifiers)
+                    Add(new ModifierDescription(ModifierTarget.Parent, stat.Key, modifier.Type, modifier.Value, modifier.overrideOrder, modifier.overrideOrderValue));
+
+            // apply all
+            foreach (ModifierDescription descrip in list) descrip.ReapplyModifier(this);
+        }
+    }
+}
--- NestedBag/ModBehaviour_NestedBag.cs
+++ NestedBag/ModBehaviour_NestedBag.cs
@@ -1,5 +1,8 @@
-﻿using ItemStatsSystem;
+﻿using Duckov.Utilities;
+using ItemStatsSystem;
 using ItemStatsSystem.Items;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace NestedBag
 {
@@ -7,26 +10,46 @@ namespace NestedBag
     {
         const int BAG_ID = 1255;
         const int EXTEND_SLOTS_COUNT = 6;
-        Slot? cachedOriginal;
+        static readonly string[] TAGS_ADD = new string[] { "TecEquip", "Totem", "Backpack", "Gem" };
 
         void OnEnable()
         {
             var bag = ItemAssetsCollection.GetPrefab(BAG_ID);
-            var slots = bag.Slots;
-            cachedOriginal = slots[0];
-            slots.Clear();
+            var go = bag.gameObject;
+
+            // slots: create new slots
+            var slots = bag.slots;
+            if (slots == null) slots = bag.slots = go.AddComponent<SlotCollection>();
+            else slots.Clear();
             for (int i = 0; i < EXTEND_SLOTS_COUNT; i++)
             {
                 slots.Add(new Slot($"bag_{i}"));
             }
+
+            // stats: exist is ok
+            var stats = bag.stats;
+            if (stats == null)
+            {
+                stats = bag.stats = go.AddComponent<StatCollection>();
+                stats.list = new List<Stat>();
+            }
+
+            // modifier: use hacked one
+            if (bag.modifiers) Destroy(bag.modifiers);
+            bag.modifiers = go.AddComponent<HackedModifiers>();
+
+            // tags
+            foreach (var tagKey in TAGS_ADD)
+            {
+                var tag = ScriptableObject.CreateInstance<Tag>();
+                tag.name = tagKey;
+                tag.show = true;
+                bag.Tags.Add(tag);
+            }
         }
         void OnDisable()
         {
-            if (cachedOriginal == null) return;
-            var bag = ItemAssetsCollection.GetPrefab(BAG_ID);
-            var slots = bag.Slots;
-            slots.Clear();
-            slots.Add(cachedOriginal);
+            // give up
         }
     }
 }
