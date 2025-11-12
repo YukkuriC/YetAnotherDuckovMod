96514e59ae9a1118de7cb11e697bc4323bf237c4
YukkuriC <799433638@qq.com>
2025-11-13 00:16:17 +0800

    强制展示直接作用于玩家的装备属性

--- NestedBag/HackedModifiers.cs
+++ NestedBag/HackedModifiers.cs
@@ -1,4 +1,6 @@
 ﻿using ItemStatsSystem;
+using ItemStatsSystem.Items;
+using ItemStatsSystem.Stats;
 using System.Collections.Generic;
 
 namespace NestedBag
@@ -6,14 +8,16 @@ namespace NestedBag
     public class HackedModifiers : ModifierDescriptionCollection
     {
         StatCollection stats;
+        SlotCollection slots;
 
         // rewrite init
         void Awake()
         {
             initialized = true;
-            if (list == null) list = new List<ModifierDescription>();
-            if (master == null) master = GetComponent<Item>();
-            if (stats == null) stats = GetComponent<StatCollection>();
+            list = new List<ModifierDescription>();
+            master = GetComponent<Item>();
+            stats = GetComponent<StatCollection>();
+            slots = GetComponent<SlotCollection>();
 
             if (master != null)
             {
@@ -40,16 +44,51 @@ namespace NestedBag
 
             // refresh all modifiers
             Clear();
+            foreach (var modDesc in GrabModDescFromStats())
+                Add(modDesc);
+
+            // apply all
+            foreach (ModifierDescription descrip in list) descrip.ReapplyModifier(this);
+
+            // collect character modifier for display
+            foreach (var modDesc in GetAllCharacterModifiersFromSlots())
+                Add(modDesc);
+        }
+
+        // helpers
+        IEnumerable<ModifierDescription> GrabModDescFromStats()
+        {
             foreach (var stat in stats)
                 foreach (var modifier in stat.Modifiers)
                 {
                     var modDesc = new ModifierDescription(ModifierTarget.Parent, stat.Key, modifier.Type, modifier.Value, modifier.overrideOrder, modifier.overrideOrderValue);
                     modDesc.display = true;
+                    yield return modDesc;
                     Add(modDesc);
                 }
-
-            // apply all
-            foreach (ModifierDescription descrip in list) descrip.ReapplyModifier(this);
+        }
+        IEnumerable<ModifierDescription> GetAllCharacterModifiersFromSlots()
+        {
+            if (slots == null) yield break;
+            foreach (var slot in slots)
+            {
+                var item = slot.Content;
+                if (item == null) continue;
+                var modifiers = item.Modifiers;
+                if (modifiers == null) continue;
+                else if (modifiers is HackedModifiers recur)
+                {
+                    foreach (var modDesc in recur.GetAllCharacterModifiersFromSlots()) yield return modDesc;
+                }
+                else
+                {
+                    foreach (var modDesc in modifiers)
+                    {
+                        if (modDesc.target != ModifierTarget.Character) continue;
+                        yield return modDesc;
+                    }
+                }
+            }
         }
     }
 }
