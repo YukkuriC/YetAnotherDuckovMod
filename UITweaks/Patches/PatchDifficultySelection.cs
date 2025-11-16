38432a0f7694f5166b17562371ef502ba164a703
YukkuriC <799433638@qq.com>
2025-11-16 15:46:53 +0800

    remove achievement limit

--- UITweaks/Patches/PatchDifficultySelection.cs
+++ UITweaks/Patches/PatchDifficultySelection.cs
@@ -1,5 +1,6 @@
 ﻿using Duckov.Rules.UI;
 using HarmonyLib;
+using UnityEngine;
 
 namespace YukkuriC.UITweaks.Patches
 {
@@ -11,6 +12,19 @@ namespace YukkuriC.UITweaks.Patches
         {
             __result = true;
             return false;
+        }
+        [HarmonyPrefix, HarmonyPatch(typeof(DifficultySelection), nameof(DifficultySelection.CustomDifficultyMarker), MethodType.Setter)]
+        static void NoDifficultyLimit_NoMarker(ref bool value)
+        {
+            value = false;
+        }
+        [HarmonyPrefix, HarmonyPatch(typeof(DifficultySelection), nameof(DifficultySelection.Execute))]
+        static void NoDifficultyLimit_NoMarker2(GameObject ___achievementDisabledIndicator, GameObject ___selectedCustomDifficultyBefore)
+        {
+            ___achievementDisabledIndicator.transform.position
+                = ___selectedCustomDifficultyBefore.transform.position
+                = new Vector3(114, 514, 1919);
+            DifficultySelection.CustomDifficultyMarker = false;
         }
     }
 }
