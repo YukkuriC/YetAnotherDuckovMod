90b4f09edd5855fcdff429b52abf8ac46c841ea7
YukkuriC <799433638@qq.com>
2025-11-16 21:59:26 +0800

    瞄准时间上个临时补丁

---
+++ UITweaks/Patches/FixADS.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace YukkuriC.UITweaks.Patches
+{
+    [HarmonyPatch]
+    public static class FixADS
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(ItemAgent_Gun), "UpdateAds")]
+        static void FixADSNaN(ref float ___adsValue)
+        {
+            if (float.IsNaN(___adsValue)) ___adsValue = 0;
+        }
+    }
+}
