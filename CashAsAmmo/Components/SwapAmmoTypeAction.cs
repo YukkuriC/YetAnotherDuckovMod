ce44941e2e9851302fe027892fa2a7747a707821
YukkuriC <799433638@qq.com>
2025-11-21 23:45:02 +0800

    可切换口径现金

---
+++ CashAsAmmo/Components/SwapAmmoTypeAction.cs
@@ -0,0 +1,34 @@
+﻿using ItemStatsSystem;
+using SodaCraft.Localizations;
+
+namespace CashAsAmmo.Components
+{
+    using static ModBehaviour;
+    public class SwapAmmoTypeAction : UsageBehavior
+    {
+        public override bool CanBeUsed(Item item, object user) => true;
+
+        int curIndex = 0;
+
+        DisplaySettingsData _cachedDisplay = new DisplaySettingsData() { display = true };
+        public override DisplaySettingsData DisplaySettings
+        {
+            get
+            {
+                _cachedDisplay.description = $"{LocalizationManager.GetPlainText(LANG_KEY_POPUP)}: {LocalizationManager.GetPlainText(NextType)}";
+                return _cachedDisplay;
+            }
+        }
+        int NextIdx => (curIndex + 1) % ALL_BULLET_TYPES.Length;
+        string NextType => ALL_BULLET_TYPES[NextIdx];
+
+        protected override void OnUse(Item master, object user)
+        {
+            var player = LevelManager.Instance?.MainCharacter;
+            if (player != null) player.PopText(DisplaySettings.description);
+            master.Constants.SetString(HASH_CALIBER, NextType);
+            curIndex = NextIdx;
+            master.StackCount++; // counters CA_UseItem.OnFinish
+        }
+    }
+}
--- CashAsAmmo/ModBehaviour_CashAsAmmo.cs
+++ CashAsAmmo/ModBehaviour_CashAsAmmo.cs
@@ -1,5 +1,8 @@
-﻿using Duckov.Utilities;
+﻿using CashAsAmmo.Components;
+using Duckov.Utilities;
 using ItemStatsSystem;
+using SodaCraft.Localizations;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CashAsAmmo
@@ -7,12 +10,18 @@ namespace CashAsAmmo
     public class ModBehaviour : Duckov.Modding.ModBehaviour
     {
         const int CASH_ID = 451;
+        public const string LANG_KEY_POPUP = "YukkuriC.CashAsAmmo.Pop";
+        public static string[] ALL_BULLET_TYPES = new string[] {
+            "AR", "BR", "SHT", "SNP", "MAG", "PWL", "PWS", "SMG"
+        };
+        public static int HASH_CALIBER = "Caliber".GetHashCode();
+
         static readonly string[] TAGS_ADD = new string[] {
             "Bullet",
         };
         static readonly CustomData[] BULLET_CONSTANTS = new CustomData[]
         {
-            new CustomData("Caliber", "AR") { Display=true },
+            new CustomData("Caliber", ALL_BULLET_TYPES[0]) { Display=true },
             new CustomData("damageMultiplier", 1f) { Display=true },
         };
 
@@ -35,6 +44,17 @@ namespace CashAsAmmo
             // constants
             foreach (var entry in BULLET_CONSTANTS) cash.Constants.Add(entry);
 
+            // using switch ammo type
+            var useItem = cash.usageUtilities = go.AddComponent<UsageUtilities>();
+            useItem.behaviors = new List<UsageBehavior>()
+            {
+                go.AddComponent<SwapAmmoTypeAction>()
+            };
+
+            // lang
+            LocalizationManager.OnSetLanguage += UpdateLang;
+            UpdateLang(LocalizationManager.CurrentLanguage);
+
             // debug code
             if (!SteamManager.Initialized)
             {
@@ -50,13 +70,31 @@ namespace CashAsAmmo
             foreach (var tagKey in TAGS_ADD) cash.Tags.Remove(CreateTagWithKey(tagKey));
             var setting = go.GetComponent<ItemSetting_Bullet>();
             if (setting != null) Destroy(setting);
-            cash.Constants.Remove(cash.Constants.GetEntry("Caliber"));
+            cash.Constants.Remove(cash.Constants.GetEntry(HASH_CALIBER));
+            Destroy(go.GetComponent<UsageUtilities>());
+            Destroy(go.GetComponent<SwapAmmoTypeAction>());
         }
         Tag CreateTagWithKey(string tagKey)
         {
             var tag = ScriptableObject.CreateInstance<Tag>();
             tag.name = tagKey;
             return tag;
+        }
+
+        void UpdateLang(SystemLanguage newLang)
+        {
+            switch (newLang)
+            {
+                default:
+                case SystemLanguage.English:
+                    LocalizationManager.SetOverrideText(LANG_KEY_POPUP, "Switch bullet type to");
+                    break;
+                case SystemLanguage.Chinese:
+                case SystemLanguage.ChineseSimplified:
+                case SystemLanguage.ChineseTraditional:
+                    LocalizationManager.SetOverrideText(LANG_KEY_POPUP, "子弹类型切换至");
+                    break;
+            }
         }
     }
 }
