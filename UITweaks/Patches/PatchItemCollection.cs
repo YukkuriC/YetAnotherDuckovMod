3529a1caf9c133ba0a6f6efe2abe5f1c324e16eb
YukkuriC <799433638@qq.com>
2025-11-24 00:17:42 +0800

    rename unknown items

---
+++ UITweaks/Patches/PatchItemCollection.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using ItemStatsSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YukkuriC.UITweaks.Patches
+{
+    [HarmonyPatch]
+    public static class PatchItemCollection
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(ItemAssetsCollection), nameof(ItemAssetsCollection.InstantiateFallbackItem))]
+        static void AssignName(Item __result)
+        {
+            __result.DisplayNameRaw = "what?";
+        }
+    }
+}
